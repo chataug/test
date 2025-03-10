211e0a10674c27eaf95aff4e4c7fbaf4e27782cb
daugier <augier5@hotmail.fr>
2025-03-11 00:26:54 +0100

    terst

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import Programs from "./Programs";
 import Contact from "./Contact";
 import './styles/global.css';
 
-function App() {
+coucou
+
+funcloliloltion App() {
   return (
     <Router>
       <div className="bg-gray-100 min-h-screen flex flex-col items-center text-gray-800">
